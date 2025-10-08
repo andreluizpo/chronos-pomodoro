68dac3bf4262b7c81fac868d17a0bb2b53df0855
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-08 10:09:22 -0400

    Criando o componente input

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Menu } from "./components/Menu";
 import "./styles/theme.css";
 import "./styles/global.css";
 import { CountDown } from "./components/CountDown";
+import { Input } from "./components/Input";
 
 export function App() {
     return (
@@ -19,6 +20,27 @@ export function App() {
 
             <Container>
                 <CountDown />
+            </Container>
+
+            <Container>
+                <form action="" className="form">
+                    <div className="formRow">
+                        <Input id="meuInput" type="text" />
+                    </div>
+
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                    <div className="formRow">
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </div>
+
+                    <div className="formRow">
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </Container>
         </>
     );
