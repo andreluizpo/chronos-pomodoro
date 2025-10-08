cc141cd0e3f05f32241131a55d399d2e413d6157
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-07 20:01:21 -0400

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -6,7 +7,12 @@ import './styles/global.css';
 export function App() {
     return (
         <>
-            <Heading attr={123} attr2='String'>Olá Mundo!1</Heading>
+            <Heading>
+                Olá Mundo!1
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur iure totam, aliquam ipsum pariatur, possimus laudantium dolores voluptates explicabo in nam voluptas labore eos fugit cupiditate tempora recusandae facere aut.</p>
         </>
     );
