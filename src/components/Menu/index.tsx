422821855bcc950178f68c6ad4f449827b6c470a
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-13 19:55:45 -0400

    Capturando o evento de clique em link com preventDefault para tema dark e light

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,55 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+    function handleThemeChange(
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    ) {
+        event.preventDefault(); // Não segue o link
+
+        console.log("clicado", Date.now());
+    }
+
     return (
         <nav className={styles.menu}>
-            <a className={styles.menuLink} href="#">
+            <h1>{theme}</h1>
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Ir para a Home"
+                title="Ir para a Home"
+            >
                 <HouseIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Ver Histórico"
+                title="Ver Histórico"
+            >
                 <HistoryIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Ir para as Configurações"
+                title="Ir para as Configurações"
+            >
                 <SettingsIcon />
             </a>
-            <a className={styles.menuLink} href="#">
+            <a
+                className={styles.menuLink}
+                href="#"
+                aria-label="Alterar Tema"
+                title="Alterar Tema"
+                onClick={handleThemeChange}
+            >
                 <SunIcon />
             </a>
         </nav>
