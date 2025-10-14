7a8708c92115f1c6ea838210a6a8af00c1d22f61
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-14 08:26:54 -0400

    Uso do useState com funções puras (sem efeito colateral) e utilização do Hook useEffect para trabalhar com efeitos colaterais no componente

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type AvailableThemes = "dark" | "light";
 
@@ -13,8 +13,26 @@ export function Menu() {
     ) {
         event.preventDefault(); // Não segue o link
 
-        console.log("clicado", Date.now());
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === "dark" ? "light" : "dark";
+            return nextTheme;
+        });
+
+        // document.documentElement.setAttribute("data-theme", theme);
     }
+
+    // useEffect(() => {
+    //     console.log("useEffect sem dependência", Date.now());
+    // }); // Executado todas as vez que o componente renderiza na tela.
+
+    // useEffect(() => {
+    //     console.log("useEffect com array de dependências vazio", Date.now());
+    // }, []); // Executa apenas quando o React monta o componente na tela pela primeira vez.
+
+    useEffect(() => {
+        console.log("Theme mudou", theme, Date.now());
+        document.documentElement.setAttribute("data-theme", theme);
+    }, [theme]); // Executa apenas quando o valor de "theme" muda.
 
     return (
         <nav className={styles.menu}>
