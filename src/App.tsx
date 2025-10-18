7c55775534312fc9cc4a2bf15012d15bce8d0873
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-18 18:55:32 -0400

    Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
 import { Home } from "./pages/Home";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
 
 import "./styles/theme.css";
 import "./styles/global.css";
 
+// export type TaskStateModel = {
+//     tasks: TaskModel[];
+//     secondsRemaining: number;
+//     formattedSecondsRemaining: string;
+//     activeTask: TaskModel | null;
+//     currentCycles: number; // 1 a 8
+//     config: {
+//         workTime: number;
+//         shortBreakTime: number;
+//         longBreakTime: number;
+//     };
+// };
+
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: "00:00",
+    activeTask: null,
+    currentCycles: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    },
+};
+
 export function App() {
-    return <Home />;
+    const [state, setState] = useState(initialState);
+
+    console.log("APP", state);
+
+    return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,31 @@ import { MainTemplate } from "../../templates/MainTemplates";
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
-export function Home() {
+type HomePros = {
+    state: TaskStateModel;
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomePros) {
+    const { state, setState } = props;
+
+    function handleClick() {
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentCycles: 5,
+            };
+        });
+    }
+
     return (
         <MainTemplate>
+            <Container>
+                <button onClick={handleClick}>clicar</button>
+            </Container>
+
             <Container>
                 <CountDown />
             </Container>
