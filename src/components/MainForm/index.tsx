d9c70b0c8a35ec3810e6cb6ae43949313419ebc4
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-23 15:58:52 -0400

    Formatando segundos em minutos e segundos (MM:SS ou 00:00)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,6 +7,7 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaksContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
+import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
 
 export function MainForm() {
     const { state, setState } = useTaskContext();
@@ -47,7 +48,8 @@ export function MainForm() {
                 activeTask: newTask,
                 currentCycles: nextCycle,
                 secondsRemaining, // Conferir
-                formattedSecondsRemaining: "00:00", // Conferir
+                formattedSecondsRemaining:
+                    formatSecondsToMinutes(secondsRemaining), // Conferir
                 tasks: [...prevState.tasks, newTask],
             };
         });
