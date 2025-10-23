69e8c05fec3ff60d15cac35f1f7dda18cdafa305
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-10-23 16:49:34 -0400

    Como iterar elementos JSX e usar a prop key no React (do jeito certo)

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -71,9 +71,11 @@ export function MainForm() {
                 <p>Próximo intervalo é de 25min</p>
             </div>
 
-            <div className="formRow">
-                <Cycles />
-            </div>
+            {state.currentCycles > 0 && (
+                <div className="formRow">
+                    <Cycles />
+                </div>
+            )}
 
             <div className="formRow">
                 <Button icon={<PlayCircleIcon />} />
