f5c81da842262a6f225fe8dc8eb7ae20958c87a3
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-11-03 14:25:46 -0400

    Criando o componente Tips para dar sugestões ao usuário sobre a tarefa e ciclo

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from "../../contexts/TaksContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
 import { TaskActionTypes } from "../../contexts/TaksContext/taskActions";
+import { Tips } from "../Tips";
 
 export function MainForm() {
     const { state, dispatch } = useTaskContext();
@@ -60,7 +61,7 @@ export function MainForm() {
             </div>
 
             <div className="formRow">
-                <p>Próximo intervalo é de 25min</p>
+                <Tips nextCycleType={nextCycleType} />
             </div>
 
             {state.currentCycles > 0 && (
---
+++ src/components/Tips/index.tsx
@@ -0,0 +1,52 @@
+import { useTaskContext } from "../../contexts/TaksContext/useTaskContext";
+
+type TipsProps = {
+    nextCycleType: "workTime" | "shortBreakTime" | "longBreakTime";
+};
+
+export function Tips({ nextCycleType }: TipsProps) {
+    const { state } = useTaskContext();
+
+    // Tips
+    const tipsForWhenActiveTask = {
+        workTime: (
+            <span>
+                Foque por <b>{state.config.workTime} minutos</b>.
+            </span>
+        ),
+        shortBreakTime: (
+            <span>
+                Descanse por <b>{state.config.shortBreakTime} minutos</b>.
+            </span>
+        ),
+        longBreakTime: (
+            <span>
+                Descanso longo de <b>{state.config.longBreakTime} minutos</b>.
+            </span>
+        ),
+    };
+
+    const tipsForNoActiveTask = {
+        workTime: (
+            <span>
+                Próximo ciclo é de <b>{state.config.workTime} minutos</b>.
+            </span>
+        ),
+        shortBreakTime: (
+            <span>
+                Próximo descanso é de <b>{state.config.shortBreakTime} minutos</b>.
+            </span>
+        ),
+        longBreakTime: (
+            <span>
+                Próximo descanso longo é de <b>{state.config.longBreakTime} minutos</b>.
+            </span>
+        ),
+    };
+    return (
+        <>
+            {!!state.activeTask && tipsForWhenActiveTask[state.activeTask.type]}
+            {!state.activeTask && tipsForNoActiveTask[nextCycleType]}
+        </>
+    );
+}
