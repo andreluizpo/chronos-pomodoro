0d388b71bc78574755bf9b44ca69de2890e48d66
André Luiz Pereira de Oliveira <156046877+andreluizpo@users.noreply.github.com>
2025-11-12 13:23:02 -0400

    Tocando áudio ao completar uma tarefa (JavaScript Audio)

--- src/contexts/TaksContext/TaskContextProvider.tsx
+++ src/contexts/TaksContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useRef } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionTypes } from "./taskActions";
+import { loadBeep } from "../../utils/loadBeep";
 
 type TaskContextProviderProps = {
     children: React.ReactNode;
@@ -11,14 +12,18 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
     const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+    const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
     const worker = TimerWorkerManager.getInstance();
 
     worker.onmessage(e => {
         const countDownSeconds = e.data;
-        console.log(countDownSeconds);
 
         if (countDownSeconds <= 0) {
+            if (playBeepRef.current) {
+                playBeepRef.current();
+                playBeepRef.current = null;
+            }
             dispatch({
                 type: TaskActionTypes.COMPLETE_TASK,
             });
@@ -32,10 +37,16 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     });
 
     useEffect(() => {
-        console.log(state);
+        if (state.activeTask && playBeepRef.current === null) {
+            playBeepRef.current = loadBeep();
+        } else {
+            playBeepRef.current = null;
+        }
+    }, [state.activeTask]);
+
+    useEffect(() => {
         if (!state.activeTask) {
             worker.terminate();
-            console.log("Worker terminado por falta de tarefa ativa.");
         }
 
         worker.postMessage(state);
---
+++ src/utils/loadBeep.ts
@@ -0,0 +1,11 @@
+import gravitationalBeep from "../assets/audios/gravitational_beep.mp3";
+
+export function loadBeep() {
+    const audio = new Audio(gravitationalBeep);
+    audio.load(); // Carrega o audio
+
+    return () => {
+        audio.currentTime = 0;
+        audio.play().catch(error => console.log("Erro ao tocar o áudio", error));
+    };
+}
